224e547857a4b26f98601027faf8a3c785f26fbc
nm1008 <mallari08nikko@yahoo.com>
2023-07-27 21:26:54 +0800

    test

---
+++ script.js
@@ -0,0 +1,49 @@
+const buttons = document.querySelectorAll("button");
+const reset = document.querySelector(".ac")
+const removeNum = document.querySelector(".delete")
+const operatorBtn = document.getElementsByClassName(".operator")
+console.log(operatorBtn)
+
+let currentNum = ""
+let previousNum = ""
+let operator = null;
+
+
+function printScreen(value){
+    const screen = document.querySelector(".screen");
+    if (value.length <= 11 ){
+        screen.innerHTML = value;
+        
+    }
+}
+
+buttons.forEach((num) => {
+  num.addEventListener("click", (e) => {
+    currentNum += e.target.value
+    printScreen(currentNum)
+  })
+})
+
+
+function resetScreen(){
+    const screen = document.querySelector(".screen");
+    screen.innerHTML = "0";
+    currentNum = ""
+}
+reset.addEventListener("click", resetScreen)
+
+
+function deleteNum(){
+    currentNum = currentNum.slice(0, -1);
+        if(currentNum === ""){
+            printScreen("0")
+        }else{
+            printScreen(currentNum)
+        }
+}
+removeNum.addEventListener("click", deleteNum)
+
+function operate(){
+    
+}
+
