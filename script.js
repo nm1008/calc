b06ee92407de14bd9b819359c0a722b66492289b
nm1008 <mallari08nikko@yahoo.com>
2023-07-28 12:47:40 +0800

    added some comments on script.js

--- script.js
+++ script.js
@@ -7,10 +7,13 @@ let currentNum = ""
 let previousNum = ""
 let operator = null;
 
-
+//prints the value that was pressed on the screen 
 function printScreen(value){
     const screen = document.querySelector(".screen");
+
+    //if statement that allows 11 digits on the screen
     if (value.length <= 11 ){
+        //if statement that prevents to print multiple zeros if the initial value is zero
         if (value === "0") {
             screen.innerHTML = "0"
         } else {
@@ -19,20 +22,21 @@ function printScreen(value){
     }
 }
 
+//button pressed on the screen 
 buttons.forEach((num) => {
     num.addEventListener("click", (e) => {
         
+        //if statement for decimal and if decimal is already included on currentNum it will just return the currentNum value
         if (e.target.value === "." && currentNum.includes(".")){
-            return;
+            return currentNum
         }
-            currentNum += e.target.value
-            printScreen(currentNum)
+        currentNum += e.target.value
+        printScreen(currentNum)
     })
 })
 
 
-
-
+//a function that resets the screen and currentNum
 function resetScreen(){
     const screen = document.querySelector(".screen");
     screen.innerHTML = "0";
@@ -40,7 +44,7 @@ function resetScreen(){
 }
 reset.addEventListener("click", resetScreen)
 
-
+//a function that deletes that last index of the number on the screen
 function deleteNum(){
     currentNum = currentNum.slice(0, -1);
         if(currentNum === ""){
@@ -50,6 +54,8 @@ function deleteNum(){
          }
     }
 removeNum.addEventListener("click", deleteNum)
+
+
 
 function operate(){
 
